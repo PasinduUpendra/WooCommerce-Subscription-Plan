3184025b06486f6fe8bbf07eb222f8e11c215d59
Pasindu Upendra <pasinduupendra@gmail.com>
2023-02-28 18:05:54 +0530

    Repository Init

---
+++ woocommerce-subscription-plan.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Subscription Plan
+ * Description: Adds a shipment plan product type to WooCommerce, which allows sellers to sell a plan that ship orders on a recurring basis.
+ * Version: 1.0
+ * Author: Pasindu Upendra
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+?>
