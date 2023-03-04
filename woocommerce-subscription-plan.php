e733f4abacf2f9464790ae82e0b13ea7f245f66e
Pasindu Upendra <pasinduupendra@gmail.com>
2023-03-04 20:12:33 +0530

    Basic functions

--- woocommerce-subscription-plan.php
+++ woocommerce-subscription-plan.php
@@ -11,4 +11,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
+// Define plugin constants.
+define( 'SUBSCRIPTION_WOOCOMMERCE_EXTENSION_VERSION', '1.0.0' );
+define( 'SUBSCRIPTION_WOOCOMMERCE_EXTENSION_PATH', plugin_dir_path( __FILE__ ) );
+define( 'SUBSCRIPTION_WOOCOMMERCE_EXTENSION_URL', plugin_dir_url( __FILE__ ) );
+
+// Load the plugin files.
+require_once SUBSCRIPTION_WOOCOMMERCE_EXTENSION_PATH . 'includes/subscription-woocommerce-extension-functions.php';
+require_once SUBSCRIPTION_WOOCOMMERCE_EXTENSION_PATH . 'includes/subscription-woocommerce-extension-shortcodes.php';
+require_once SUBSCRIPTION_WOOCOMMERCE_EXTENSION_PATH . 'includes/subscription-woocommerce-extension-admin.php';
+
 ?>
